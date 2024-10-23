b9fcee797df695d74867d8bdaf0f5f3211bb15b7
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-10-23 16:15:12 -0300

    table rendering data and material module organization

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  displayedColumns = ['id', 'name', 'category'];
 
-  constructor() { }
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Frontend' },
+    { _id: '2', name: 'Java', category: 'Backend' },
+    { _id: '3', name: 'SQL', category: 'Backend' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
