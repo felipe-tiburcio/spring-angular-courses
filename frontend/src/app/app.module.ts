f1df31d93d343572bcbc759c18179a826256c924
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-10-25 23:19:54 -0300

    error dialog popup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     AppMaterialModule,
     HttpClientModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,9 +16,24 @@ export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((err) => {
+        this.onError(err.message);
+
+        return of([]);
+      })
+    );
   }
 
   ngOnInit(): void {}
+
+  onError(msg: string) {
+    this.dialog.open(ErrorDialogComponent,{
+      data: msg
+    })
+  }
 }
