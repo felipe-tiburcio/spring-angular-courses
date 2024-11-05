563e3efd0707b0770dc885ba7e43b3d6a65cd2f6
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-11-05 15:03:11 -0300

    form to new course with save and cancel button

--- frontend/src/app/courses/course-form/course-form.component.ts
+++ frontend/src/app/courses/course-form/course-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      name: [],
+      category: [],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+    this.router.navigate(["/courses"]);
+  }
 }
