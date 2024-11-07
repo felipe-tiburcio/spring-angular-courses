dbe417384a4ec6d7195dadeb79ff6ab057932a24
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-11-07 15:28:55 -0300

    angular core update version 14

--- frontend/src/app/courses/course-form/course-form.component.ts
+++ frontend/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,10 +10,10 @@ import { Location } from '@angular/common';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-  form: FormGroup;
+  form: UntypedFormGroup;
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: UntypedFormBuilder,
     private router: Router,
     private service: CoursesService,
     private _snackBar: MatSnackBar,
